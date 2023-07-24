50d33e1d5f3a37675a4a4e593407af58efa6484e
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-07-24 20:10:27 +0300

    export from api as json

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,15 +7,15 @@ import sys
 
 
 if __name__ == '__main__':
-    id_c = sys.argv[1]
-    url_user = "https://jsonplaceholder.typicode.com/users/" + id_c
+    id = sys.argv[1]
+    url_user = "https://jsonplaceholder.typicode.com/users/" + id
     res = requests.get(url_user).json()
     username = res.get("username")
     req = requests.get(
         'https://jsonplaceholder.typicode.com/users/' +
-        (id_c) + '/todos')
+        (id) + '/todos')
     with open("{}.csv".format(sys.argv[1]), "w") as file_c:
         writer = csv.writer(file_c, quoting=csv.QUOTE_ALL)
         for task in req.json():
-            writer.writerow([id_c, username,
+            writer.writerow([id, username,
                             task.get("completed"), task.get("title")])
